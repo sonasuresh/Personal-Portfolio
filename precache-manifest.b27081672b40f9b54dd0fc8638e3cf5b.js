242ee95baf2767ab5eccc787048f8235f58ce34f
sona.suresh158 <sona.suresh158@gmail.com>
2023-03-27 13:42:05 +0530

    Updates

---
+++ precache-manifest.b27081672b40f9b54dd0fc8638e3cf5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb97dbccd4f0b9c68bc2b073668132e8",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "64aa2dfadbdcc3ae10e5",
+    "url": "/react-gh-pages/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "a9630391b9d6c63f6561",
+    "url": "/react-gh-pages/static/css/main.ec057646.chunk.css"
+  },
+  {
+    "revision": "64aa2dfadbdcc3ae10e5",
+    "url": "/react-gh-pages/static/js/2.ce54fdb4.chunk.js"
+  },
+  {
+    "revision": "d222a6bf1f20a915b20b26e6afc4121e",
+    "url": "/react-gh-pages/static/js/2.ce54fdb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9630391b9d6c63f6561",
+    "url": "/react-gh-pages/static/js/main.aaeb2964.chunk.js"
+  },
+  {
+    "revision": "ddcd8e56a53d0e7fcb77",
+    "url": "/react-gh-pages/static/js/runtime-main.618ca992.js"
+  },
+  {
+    "revision": "3ae5b2da9f3895f2d8301a180c4d626f",
+    "url": "/react-gh-pages/static/media/6.3ae5b2da.png"
+  }
+]);
